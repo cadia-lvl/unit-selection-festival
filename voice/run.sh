1e8c1553f29e56eaf260ff3aa155346b323b0213
judyfong <judyyfong@gmail.com>
2020-11-12 20:21:52 +0000

    Fix spelling mistakes and comments for future steps

--- voice/run.sh
+++ voice/run.sh
@@ -1,11 +1,14 @@
 # Make some dirs we need
 $FESTVOXDIR/src/general/make_dirs
 
-# Power normalize and fromat wavs (16kHz, 16bit)
+# Power normalize and format wavs (16kHz, 16bit)
 bin/get_wavs $1/audio/*.wav
 
 # Set up the prompts that we will train on.
 # ---
+
+# TODO: filter out prompts with unsupported letters
+# then use that for creating txt.complete.data and vocabulary.txt
 
 # Create transcriptions for everything
 python3 normalize.py $1/index.tsv txt.complete.data --scm
@@ -13,13 +16,15 @@ python3 normalize.py $1/index.tsv txt.complete.data --scm
 # Add random noise to audio (see script for more info)
 bin/add_noise txt.complete.data
 
-# Filter out prompts with numbers or unsupported litters (c, w, q and z)
+# Filter out prompts with numbers or unsupported letters (c, w, q and z)
 grep -v '"[^"]*[0-9cwqz]' txt.complete.data > txt.nonum.data
 
 # For large databases this can take some time to run as there is a squared aspect 
 # to this based on the number of instances of each unit type.
-# So lets start with only 1000 tokens, this number can be increased for better sound
-head -n 100 txt.nonum.data > etc/txt.done.data
+# So lets start with only 100 tokens, this number can be increased for better sound
+ head -n 100 txt.nonum.data > etc/txt.done.data
+# Using all the tokens uncomment the line below if you only want to use a subset of the tokens
+# cp -p txt.nonum.data etc/txt.done.data
 
 # Create a lexicon
 # ---
