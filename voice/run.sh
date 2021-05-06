5b68ee0b69aa246be897fbf61a8f391d89b0b674
Gunnar Thor <ornolfsson@gmail.com>
2021-05-06 16:51:08 +0000

    uncommented lines in run.sh and reintroduced generate_statenames step

--- voice/run.sh
+++ voice/run.sh
@@ -76,16 +76,21 @@ mv festival/dur/etc/statedur.feats-backup festival/dur/etc/statedur.feats
 ./bin/do_build label
 ./bin/do_build build_utts
 
-# ./bin/do_clustergen parallel f0
-# ./bin/do_clustergen parallel mcep
-# ./bin/do_clustergen parallel voicing
-# ./bin/do_clustergen parallel combine_coeffs_v
+./bin/do_clustergen parallel f0
+./bin/do_clustergen parallel mcep
+./bin/do_clustergen parallel voicing
+./bin/do_clustergen parallel combine_coeffs_v
 
-# ./bin/traintest etc/txt.done.data
-# cp etc/txt.done.data etc/text.done.data.full
-# cat etc/txt.done.data.train >etc/txt.done.data
+./bin/traintest etc/txt.done.data
+cp etc/txt.done.data etc/text.done.data.full
+cat etc/txt.done.data.train >etc/txt.done.data
 
-# # ./bin/do_clustergen generate_statenames
-# ./bin/do_clustergen generate_filters
-# ./bin/do_clustergen cluster
-# ./bin/do_clustergen dur
+
+cp festival/clunits/mcep.desc festival/clunits/mcep.desc-backup
+cp festival/clunits/mceptraj.desc festival/clunits/mceptraj.desc-backup
+./bin/do_clustergen generate_statenames
+mv festival/clunits/mcep.desc-backup festival/clunits/mcep.desc
+mv festival/clunits/mceptraj.desc-backup festival/clunits/mceptraj.desc
+./bin/do_clustergen generate_filters
+./bin/do_clustergen cluster
+./bin/do_clustergen dur
